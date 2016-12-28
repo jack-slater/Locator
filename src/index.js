fe01ddf328b88cbd881d8e74f8eb169da802a813
slaterJJ <jackslater@hotmail.com>
2016-12-28 14:14:55 +0000

    Chnage to use more than one component

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { Coordinates } from './containers';
 
-export class Locator extends Component {
-  constructor () {
-    super();
-    this.state = {
-      location: 'unknown'
-    }
-  }
-
-  showLocation () {
-    navigator.geolocation.getCurrentPosition(pos => {
-      const location = JSON.stringify(pos);
-      this.setState({location});
-    },
-    error => console.warn(JSON.stringify(error)),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-    );
-  }
-
-  render() {
-    return (
-      <View>
-        <TouchableOpacity onPress={this.showLocation.bind(this)}>
-          <Text>Press Me</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>{this.state.props}</Text>
-        </View>
-      </View>
-    );
-  }
-}
+export const Root = () => (
+    <Coordinates />
+);
