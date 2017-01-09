5fcb1726b4e849c81d97e96f2d0f694e00d6df72
Freddie Freeston <freddiefreeston@Freddies-MacBook-Pro.local>
2017-01-09 18:05:10 +0000

    Add seed file, data and schemas

---
+++ models/sightings.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const SightingSchema = new Schema({
+  park_id: {
+    type: Number,
+    required: true
+  },
+  animal_id: {
+    type: Number,
+    required: true
+  },
+  animal_name: {
+    type: String,
+    required: true
+  },
+  lat_lng: {
+    latitude: {
+      type: Number,
+      required: true
+    },
+    longitude: {
+      type: Number,
+      required: true
+    }
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  spatial_ref: {
+    type: String
+  },
+  obs_comment: {
+    type: String,
+    default: ''
+  },
+  obs_abundance: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  obs_photo: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('sightings', SightingSchema);
---
+++ seed/data/animals.js
@@ -0,0 +1,322 @@
+module.exports = [
+ {
+   taxon_group: "Bird",
+   latin_name: "Aix galericulata",
+   common_name: "Mandarin Duck"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Turdus iliacus",
+   common_name: "Redwing"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Columba livia (feral)",
+   common_name: "Feral Pigeon"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Aythya fuligula",
+   common_name: "Tufted Duck"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Mergus merganser",
+   common_name: "Goosander"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Branta canadensis",
+   common_name: "Canada Goose"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Anas platyrhynchos",
+   common_name: "Mallard"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Columba palumbus",
+   common_name: "Woodpigeon"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Anser brachyrhynchus",
+   common_name: "Pink-footed Goose"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Chroicocephalus ridibundus",
+   common_name: "Black-headed Gull"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Pica pica",
+   common_name: "Magpie"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Aegithalos caudatus",
+   common_name: "Long-tailed Tit"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Psittacula krameri",
+   common_name: "Ring-necked Parakeet"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Gallinula chloropus",
+   common_name: "Moorhen"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Turdus merula",
+   common_name: "Blackbird"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Turdus viscivorus",
+   common_name: "Mistle Thrush"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Carduelis carduelis",
+   common_name: "Goldfinch"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Cyanistes caeruleus",
+   common_name: "Blue Tit"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Fulica atra",
+   common_name: "Coot"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Sitta europaea",
+   common_name: "Nuthatch"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Dendrocopos major",
+   common_name: "Great Spotted Woodpecker"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Parus major",
+   common_name: "Great Tit"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Regulus regulus",
+   common_name: "Goldcrest"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Erithacus rubecula",
+   common_name: "Robin"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Anas clypeata",
+   common_name: "Shoveler"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Cygnus olor",
+   common_name: "Mute Swan"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Garrulus glandarius",
+   common_name: "Jay"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Certhia familiaris",
+   common_name: "Treecreeper"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Troglodytes troglodytes",
+   common_name: "Wren"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Corvus corone",
+   common_name: "Carrion Crow"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Columba oenas",
+   common_name: "Stock Dove"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Turdus philomelos",
+   common_name: "Song Thrush"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Ardea cinerea",
+   common_name: "Grey Heron"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Bombycilla garrulus",
+   common_name: "Waxwing"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Carduelis spinus",
+   common_name: "Siskin"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Sylvia atricapilla",
+   common_name: "Blackcap"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Accipiter nisus",
+   common_name: "Sparrowhawk"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Regulus ignicapilla",
+   common_name: "Firecrest"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Fringilla coelebs",
+   common_name: "Chaffinch"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Carduelis chloris",
+   common_name: "Greenfinch"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Motacilla cinerea",
+   common_name: "Grey Wagtail"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Pyrrhula pyrrhula",
+   common_name: "Bullfinch"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Turdus pilaris",
+   common_name: "Fieldfare"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Periparus ater",
+   common_name: "Coal Tit"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Prunella modularis",
+   common_name: "Dunnock"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Phalacrocorax carbo",
+   common_name: "Cormorant"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Phylloscopus collybita",
+   common_name: "Chiffchaff"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Tachybaptus ruficollis",
+   common_name: "Little Grebe"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Streptopelia decaocto",
+   common_name: "Collared Dove"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Sturnus vulgaris",
+   common_name: "Starling"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Larus canus",
+   common_name: "Common Gull"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Corvus monedula",
+   common_name: "Jackdaw"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Passer domesticus",
+   common_name: "House Sparrow"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Muscicapa striata",
+   common_name: "Spotted Flycatcher"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Motacilla alba yarrellii",
+   common_name: "Pied Wagtail"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Anser anser",
+   common_name: "Greylag Goose"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Aythya ferina",
+   common_name: "Pochard"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Podiceps cristatus",
+   common_name: "Great Crested Grebe"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Strix aluco",
+   common_name: "Tawny Owl"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Phylloscopus trochilus",
+   common_name: "Willow Warbler"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Fringilla montifringilla",
+   common_name: "Brambling"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Larus fuscus",
+   common_name: "Lesser Black-backed Gull"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Larus argentatus",
+   common_name: "Herring Gull"
+ },
+ {
+   taxon_group: "Bird",
+   latin_name: "Sylvia borin",
+   common_name: "Garden Warbler"
+ }
+];
