8413d38bfc99275840d306789d0063b8872c22b6
slaterJJ <jackslater@hotmail.com>
2017-01-09 17:54:48 +0000

    Add modal to change settings

---
+++ src/containers/Drawer.js
@@ -0,0 +1,37 @@
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  DrawerLayoutAndroid,
+  TouchableHighlight
+} = React;
+
+export class Drawer extends React.createClass({
+  openDrawer:function() {
+    this.refs['DRAWER'].openDrawer()
+  },
+  render: function() {
+    var navigationView = (
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+        </View>
+    );
+    return (
+          <DrawerLayoutAndroid
+              drawerWidth={300}
+              ref={'DRAWER'}
+              drawerPosition={DrawerLayoutAndroid.positions.Left}
+              renderNavigationView={() => navigationView}>
+            <View style={{flex: 1, alignItems: 'center'}}>
+              <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
+              <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
+              <TouchableHighlight onPress={this.openDrawer}>
+                <Text>{'Open Drawer'}</Text>
+              </TouchableHighlight>
+            </View>
+          </DrawerLayoutAndroid>
+    );
+  }
+})
